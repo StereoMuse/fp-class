7279c5f4b958397917ee957a19f344a79d0947d7
StereoMuse <KolesnikovaK7@mail.rugit status>
2014-11-10 21:03:57 +0300

    Kolesnikova, class09

--- class-09/02-reduce.hs
+++ class-09/02-reduce.hs
@@ -1,40 +1,55 @@
 import System.Environment
+import System.Random
+import Data.Maybe
 
 {-
-  Напишите функцию reduce, принимающую один целочисленный аргумент a и возвращающую 0,
-  если аргумент делится на 3, a^2, если он на 3 не делится и является при этом нечётным,
+  Напишите функцию reduce, принимающую один целочисленный аргумент a и возвращающую 
+  0, если аргумент делится на 3, 
+  a^2, если он на 3 не делится и является при этом нечётным,
   a^3 в остальных случаях.
 -}
 
 reduce :: Integral a => a -> a
-reduce = undefined
+reduce a
+	| a `mod` 3 == 0 = 0
+	| odd a = a^2
+	| otherwise = a^3
 
 {-
-  Напишите функцию, применяющую функцию reduce заданное количество раз к значению в контексте,
-  являющемся функтором:
+  Напишите функцию, применяющую функцию reduce заданное количество раз к 
+  значению в контексте, являющемся функтором:
 -}
 
 reduceNF :: (Functor f, Integral a) => Int -> f a -> f a
-reduceNF = undefined
+reduceNF 0 a = a
+reduceNF n a = reduceNF (n-1) (fmap reduce a)
 
 {-
-  Реализуйте следующие функции-преобразователи произвольным, но, желательно, осмысленным и
-  нетривиальным способом.
+  Реализуйте следующие функции-преобразователи произвольным, но, желательно, 
+  осмысленным и нетривиальным способом.
 -}
 
 toList :: Integral a => [(a, a)]  -> [a]
-toList = undefined
+toList = foldr (\(x,y) acc -> (x + y + 42) : acc) []
 
 toMaybe :: Integral a => [(a, a)]  -> Maybe a
-toMaybe = undefined
+toMaybe [] = Nothing
+toMaybe xs = Just (maximum  $ toList xs)
 
 toEither :: Integral a => [(a, a)]  -> Either String a
-toEither = undefined
+toEither [] = Left "Nothing"
+toEither xs = Right (maximum $ toList xs)
 
 -- воспользуйтесь в этой функции случайными числами
 toIO :: Integral a => [(a, a)]  -> IO a
-toIO = undefined
-
+toIO xs = do
+	gen <- newStdGen
+	let (i, genn) = randomR (0,length xs -1 ) gen::(Int,StdGen)
+	let (f,s) = (!!) xs i
+	let xss = if f > s then [s..f] else [f..s]
+	let (ii, gen) = randomR (0,length xss-1) genn::(Int,StdGen)
+	return $ (!!) xss ii
+	
 {-
   В параметрах командной строки задано имя текстового файла, в каждой строке
   которого записана пара целых чисел, разделённых пробелами. Загрузите
@@ -45,19 +60,40 @@ toIO = undefined
 -}
 
 parseArgs :: [String] -> (FilePath, Int)
-parseArgs = undefined
+parseArgs s = (head s, read $ last s)
 
 readData :: FilePath -> IO [(Int, Int)]
-readData = undefined
+readData filename = do
+	content <- readFile filename
+	return $ map (\s -> let [f',s'] = words s in (read f', read s')) $ lines content
 
 main = do
-  (fname, n) <- parseArgs `fmap` getArgs
-  ps <- readData fname
-  undefined
+  (filename, n) <- parseArgs `fmap` getArgs
+  ps <- readData filename
+  print $ toList ps
+  print $ reduceNF n (toList ps)
+  print $ reduceNF n (toMaybe ps)
   print $ reduceNF n (toEither ps)
   reduceNF n (toIO ps) >>= print
 
 {-
   Подготовьте несколько тестовых файлов, демонстрирующих особенности различных контекстов.
   Скопируйте сюда результаты вызова программы на этих файлах.
+
+файл "b.txt":
+	2 5
+	4 8
+	2 6
+	4 4
+	1 3
+	8 2
+	9 1
+
+результат:
+	*Main> :main "b.txt" 0
+	[49,54,50,50,46,52,52]
+	[49,54,50,50,46,52,52]
+	Just 54
+	Right 54
+	5
 -}
