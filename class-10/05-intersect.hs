f572bf9a747e252417072d47dedd84b79d73655f
StereoMuse <KolesnikovaK7@mail.rugit status>
2014-11-18 00:40:16 +0300

    Kolesnikova class-10

--- class-10/05-intersect.hs
+++ class-10/05-intersect.hs
@@ -1,3 +1,13 @@
-{- Пользуясь списком как монадой, вычислите пересечение  заданных списков -}
+{- Пользуясь списком как монадой, вычислите пересечение заданных списков -}
+
+import Control.Monad
+
 intersect :: Eq a => [[a]] -> [a]
-intersect = foldr undefined undefined
+intersect [] = []
+intersect (x : xs) = foldr (\acc y -> return y >>= filter (`elem` acc)) x xs
+
+{-  test1
+	test2
+-}
+test1 = intersect [[1],[2]]
+test2 = intersect [[1,2],[2,3]]
